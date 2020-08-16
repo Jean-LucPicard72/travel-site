bcd1620a9665068c9e8fc8e08ae1f9586bc49e87
“Jean-LucPicard72” <rusadskiy@ya.ru>
2020-08-16 21:55:31 +0400

    Revealing Elements on Scroll (Part 2)

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -4,27 +4,42 @@ class RevealOnScroll {
     constructor() {
         this.itemsToReveal = document.querySelectorAll(".feature-item")
         this.hideInitially()
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
         this.events()
     }
 
     events() {
-        window.addEventListener("scroll", () => {
+        window.addEventListener("scroll", this.scrollThrottle)
+    }
+
+    calcCaller() {
+        console.log("Scroll function ran")
             this.itemsToReveal.forEach(el => {
-                this.calculateIfScrolledTo(el)
+                if (el.isRevaeled == false) {
+                    this.calculateIfScrolledTo(el)
+                }
             })
-        })
     }
 
     calculateIfScrolledTo(el) {
+        console.log
         let scrollPercent = (el.getBoundingClientRect().y / window.innerHeight) * 100
         if (scrollPercent < 75) {
             el.classList.add("reveal-item--is-visible")
+            el.isRevaeled = true
+            if (el.isLastItem) {
+                window.removeEventListener("scroll", this.scrollThrottle)
+            }
         }
     }
 
 
     hideInitially() {
-        this.itemsToReveal.forEach(el => el.classList.add("reveal-item"))
+        this.itemsToReveal.forEach(el => {
+            el.classList.add("reveal-item")
+            el.isRevaeled = false
+        })
+        this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true
     }
     
 }
