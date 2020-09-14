3a0756dbc7a3a0e0d5942dda4545b9852ed4837e
“Jean-LucPicard72” <rusadskiy@ya.ru>
2020-09-14 16:55:35 +0400

    complited modal

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -11,14 +11,25 @@ class Modal {
         // listen for open click
         this.openModalButtons.forEach(el => el.addEventListener("click", e => this.openTheModal(e)))
         // listen for close click
-        this.closeIcon.addEventListener("click", () => this.closeTheModal)
+        this.closeIcon.addEventListener("click", () => this.closeTheModal())
 
         // pushes any key
+        document.addEventListener("keyup", e =>this.keyPressHandler(e))
+    }
+
+    keyPressHandler(e) {
+      if (e.keyCode ==27) {
+        this.closeTheModal()
+      }
     }
 
     openTheModal(e) {
       e.preventDefault()
       this.modal.classList.add("modal--is-visible")
+    }
+
+    closeTheModal() {
+      this.modal.classList.remove("modal--is-visible")
     }
 
     injectHTML() {
